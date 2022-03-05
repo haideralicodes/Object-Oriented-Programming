3204a9c130e1119789414158bfe3c0aa866f16d5
Haider Ali <haiderali920687@gmail.com>
2022-03-05 12:37:27 +0500

    Runner.java File updated
    updated code of Runner class

--- Code/Lab Task 3/Runner.java
+++ Code/Lab Task 3/Runner.java
@@ -2,62 +2,79 @@ public class Runner{
     public static void main(String args[]){
         
          // Q1
-//          Account a1 = new Account();
-//          a1.setValues(1200, "1234567891234", 2022);
-//          a1.display();
-         // a1.Account();                               Default Constructor
-         // a1.Account(1500);                           One-Argument Constructor
-         // a1.Account(2400, "3452348976345", 2021);    Three-Argument Constructor
-//          a1.deposit(1000);
-//          a1.withdraw(500);
-//          a1.calculateAgeOfAccount(2022);
+          Account a1 = new Account();
+          a1.setValues(1200, "1234567891234", 2022);
+          a1.display();
+          a1.Account();                                         //Default Constructor
+          a1.Account(1500);                                 //One-Argument Constructor
+          a1.Account(2400, "3452348976345", 2021);    //Three-Argument Constructor
+          a1.deposit(1000);
+          a1.withdraw(500);
+          a1.calculateAgeOfAccount(2022);
+
+        System.out.println();
+        System.out.println();
 
           // Q2
-//        Book b1 = new Book();
-//        String[] chName = {"1", "2", "3", "4", "5"};
-//        b1.setValues("Ali", chName);
-//        b1.display();
-//        b1.checkIfAuthorNameStartsWithA();
-//        b1.searchChapter("1");
+        Book b1 = new Book();
+        String[] chName = {"1", "2", "3", "4", "5"};
+        b1.setValues("Ali", chName);
+        b1.display();
+        b1.checkIfAuthorNameStartsWithA();
+        b1.searchChapter("1");
 
+        System.out.println();
+        System.out.println();
 
           //Q3
-//        Circle c1 = new Circle();
-//        c1.setValues(4.21);
-//        c1.display();
-//        c1.findDiameter();
-//        c1.findCircumfrence();
-//        c1.findArea();
+        Circle c1 = new Circle();
+        c1.setValues(4.21);
+        c1.display();
+        c1.findDiameter();
+        c1.findCircumfrence();
+        c1.findArea();
+
+        System.out.println();
+        System.out.println();
 
         // Q4
-//        Point p1 = new Point();
-//        p1.setValues(1, 4);
-//        p1.display();
-//        p1.move(3, 5);
-//        p1.checkOrigin();
+        Point p1 = new Point();
+        p1.setValues(1, 4);
+        p1.display();
+        p1.move(3, 5);
+        p1.checkOrigin();
+
+        System.out.println();
+        System.out.println();
         
         // Q5
-//        QuadraticEquation q1 = new QuadraticEquation();
-//        q1.setValues(2.5, 5.1, 10.6);
-//        q1.display();
-//        q1.getDiscriminant();
-//        q1.checkIfDescriminantIsGretaerThan100();
+        QuadraticEquation q1 = new QuadraticEquation();
+        q1.setValues(2.5, 5.1, 10.6);
+        q1.display();
+        q1.getDiscriminant();
+        q1.checkIfDescriminantIsGretaerThan100();
+
+        System.out.println();
+        System.out.println();
 
         //Q6
-//        Rectangle r1 = new Rectangle();
-//        r1.setValues(23, 6);
-//        r1.display();
-//        r1.calculateArea();
-//        r1.checkSquare();
+        Rectangle r1 = new Rectangle();
+        r1.setValues(23, 6);
+        r1.display();
+        r1.calculateArea();
+        r1.checkSquare();
+
+        System.out.println();
+        System.out.println();
 
         // Q7
-//         Student s1 = new Student();
-//         String[] subj = {"bio", "physics", "chemistry", "urdu", "math"};
-//         s1.setValues("ali", 3.6, subj, "ali@gmail.com");
-//         s1.display();
-//         s1.searchSubject("chemistry");
-//         s1.checkProbStatus();
-//         s1.validateEmail();
+        Student s1 = new Student();
+        String[] subj = {"bio", "physics", "chemistry", "urdu", "math"};
+        s1.setValues("ali", 3.6, subj, "ali@gmail.com");
+        s1.display();
+        s1.searchSubject("chemistry");
+        s1.checkProbStatus();
+        s1.validateEmail();
 
 
     }
